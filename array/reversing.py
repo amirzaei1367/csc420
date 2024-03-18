afd68bdda03dffa321a37fe8c7b419f3727e378a
Ali Mirzaeian <56352498+amirzaei1367@users.noreply.github.com>
2024-03-18 15:57:17 -0400

    Update reversing.py

--- array/reversing.py
+++ array/reversing.py
@@ -1,2 +1,7 @@
+'''
+In this exercise, you have to reverse a list in O(N) linear time complexity and we want the algorithm to be in-place as well - so the algorithm can not use additional memory 
+(it means you have to manipulate the input list and not create an independent list)!
+For example: input is [1,2,3,4,5] then the output is [5,4,3,2,1]
+'''
 def reverse(nums):
   pass
