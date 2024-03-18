e570d8b531d2a9f6a5f5a1420167d2402682ddeb
Ali Mirzaeian <56352498+amirzaei1367@users.noreply.github.com>
2024-03-18 17:34:20 -0400

    Update anagram.py

--- array/anagram.py
+++ array/anagram.py
@@ -7,6 +7,4 @@ Construct an algorithm to check whether two words (or phrases) are anagrams or n
 For example: restful and fluster
 '''
 def is_anagram(str1, str2):
-
-def is_anagram(str1, str2):
   pass
