cbf9f27515f0496a3a5400ff6770c7fadbcbc32b
Ali Mirzaeian <56352498+amirzaei1367@users.noreply.github.com>
2024-03-18 17:34:02 -0400

    Create anagram.py

---
+++ array/anagram.py
@@ -0,0 +1,12 @@
+'''
+Construct an algorithm to check whether two words (or phrases) are anagrams or not!
+
+"An anagram is a word or phrase formed by rearranging the letters of a different
+ word or phrase, typically using all the original letters exactly once"
+
+For example: restful and fluster
+'''
+def is_anagram(str1, str2):
+
+def is_anagram(str1, str2):
+  pass
