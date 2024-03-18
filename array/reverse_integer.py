9279f657621aac509259f1dca4a53a421888b428
Ali Mirzaeian <56352498+amirzaei1367@users.noreply.github.com>
2024-03-18 17:32:08 -0400

    Create reverse_integer.py

---
+++ array/reverse_integer.py
@@ -0,0 +1,8 @@
+'''
+Your task is to design an efficient algorithm to reverse a given integer.
+For example if the input of the algorithm is 1234 then the output should be 4321.
+NOTE: the input is an integer (and not a string) !!!
+'''
+
+def reverse_integer(n):
+  pass
