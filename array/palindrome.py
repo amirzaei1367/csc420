9a13da43a273c985e6d37533cd87fdc855060b5e
Ali Mirzaeian <56352498+amirzaei1367@users.noreply.github.com>
2024-03-18 17:31:16 -0400

    Update palindrome.py

--- array/palindrome.py
+++ array/palindrome.py
@@ -1,1 +1,7 @@
-
+'''
+"A palindrome is a string that reads the same forward and backward"
+For example: radar or madam
+Our task is to design an algorithm for checking whether a given string is palindrome or not! The aim is to achieve O(N) linear running time.
+'''
+def is_palindrome(s):
+  pass
