1048c4103af0b196754c68d2097ffe56bd627aa2
Ali Mirzaeian <56352498+amirzaei1367@users.noreply.github.com>
2024-03-18 21:40:13 -0400

    Create midle_node.py

---
+++ linklist/midle_node.py
@@ -0,0 +1,5 @@
+'''Suppose we have a standard linked list. Construct an in-place (without extra memory) algorithm that is able to find the middle node - and return with the middle node!
+
+Note: you should construct an O(N) linear running time algorithm
+
+For example: [1, 2, 3, 4] --> middle node is: 2,   [1, 2, 3, 4, 5] --> middle node is: 3'''
