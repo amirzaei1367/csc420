b549c908ce2c8abeade4130d0d7368921d31c8c3
Ali Mirzaeian <56352498+amirzaei1367@users.noreply.github.com>
2024-03-25 12:25:57 -0400

    Create queue_with_stack_rec.py

---
+++ stack/queue_with_stack_rec.py
@@ -0,0 +1,29 @@
+
+
+class Queue:
+    def __init__(self):
+        self.stack_enqueue = []
+
+    def enqueue(self, data):
+        self.stack_enqueue.append(data)
+
+    def dequeue(self):
+        if len(self.stack_enqueue) == 1:
+            return self.stack_enqueue.pop()
+
+        temp = self.stack_enqueue.pop()
+        target = self.dequeue()
+        self.stack_enqueue.append(temp)
+        return target
+
+    def size(self):
+        return len(self.stack_enqueue)
+
+
+q = Queue()
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
